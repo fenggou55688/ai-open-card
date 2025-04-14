ea5c5adfcf9204b5be0b589d7ae2598b67325db0
fenggou55688 <jason0971545575@gmail.com>
2025-04-14 21:54:44 +0800

    Create historyData.js

---
+++ src/historyData.js
@@ -0,0 +1,13 @@
+// 模擬 5000 局歷史結果
+const outcomes = ['莊', '閒', '和'];
+
+export const generateHistoryData = (length = 5000) => {
+  const history = [];
+  for (let i = 0; i < length; i++) {
+    const rand = Math.random();
+    if (rand < 0.45) history.push('莊');
+    else if (rand < 0.9) history.push('閒');
+    else history.push('和');
+  }
+  return history;
+};
